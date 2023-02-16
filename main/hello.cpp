c72a8ec195df4e09e5e7a82c42ed19e4b9656b21
ayushikuma <80847369+ayushikuma@users.noreply.github.com>
2023-02-16 21:43:30 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 
 int main(void) {
+  cout << "hii\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "pes2ug20cs587\n";
   return 0;
 }
